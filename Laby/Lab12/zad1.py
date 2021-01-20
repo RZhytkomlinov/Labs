46b19e9b1dac3775bd38630a0fe1cdd6ca918de9
RZhytkomlinov <rzhytkomlinov@gmail.com>
2021-01-20 12:05:22 +0100

    zadania

--- Laby/Lab12/zad1.py
+++ Laby/Lab12/zad1.py
@@ -1,19 +1,47 @@
-import math
+class Calc:
+    def suma(x, y):
+        return x + y
 
-a = int(input("Daj 1 liczbe: "))
-b = int(input("Daj 2 liczbe: "))
+    def roznica(x, y):
+        return x - y
 
-print("1 - Dodawanie"
-      "2 - Odejmowanie"
-      "3 - Mnożenie"
-      "4 - Dzielenie"
-      "5 - Pierwiastkowanie"
-      )
+    def mnozenie(x, y):
+        return x * y
 
+    def dzielenie(x, y):
+        return x / y
 
-def dzialania(a, b):
-    finish = f'+: {a + b},-: {a - b},*: {a * b},/: {a / b},sqrt: {math.sqrt(a)}, {math.sqrt(b)}'
-    return finish
+    def pierwiastek(x):
+        return x ** 0.5
 
 
-print(dzialania(a, b))
+print("""
+1 - Dodawanie
+2 - Odejmowanie
+3 - Mnożenie
+4 - Dzielenie
+5 - Pierwiastek Kwadratowy
+""")
+
+wybor = int(input("Podaj jakie działanie chcesz wykonać: "))
+
+if wybor != 5:
+    inp1 = float(input("Podaj pierwszą liczbę: "))
+    inp2 = float(input("Podaj drugą liczbę: "))
+else:
+    inp1 = float(input("Podaj pierwszą liczbę: "))
+
+if wybor == 1:
+    print(Calc.suma(inp1, inp2))
+
+elif wybor == 2:
+    print(Calc.roznica(inp1, inp2))
+
+elif wybor == 3:
+    print(Calc.mnozenie(inp1, inp2))
+
+elif wybor == 4:
+    print(Calc.dzielenie(inp1, inp2))
+
+elif wybor == 5:
+    print(Calc.pierwiastek(inp1))
--- Laby/Lab12/zad3.py
+++ Laby/Lab12/zad3.py
@@ -1,31 +1,41 @@
-# def cal(cal):
-#     print(f"Wysokość w m {wynik / 100}")
-#     print(f"Wysokość w cm {wynik}")
-#     print(f"Wysokość w mm {wynik * 10}")
-#     print(f"Wysokość w km {wynik / 1000}")
-cal = int(input("Podaj cale: "))
-
-cale = 2.54*cal
 def cm(cale):
-    finish = f'W cm: {cale/10}'
+    finish = f'W cm: {cale / 10}'
     return finish
+
+
 def mm(cale):
-    finish = f'W mm: {cale*10}'
+    finish = f'W mm: {cale * 10}'
     return finish
+
+
 def m(cale):
     finish = f'W m: {cale}'
     return finish
+
+
 def km(cale):
-    finish = f'W km: {cale/1000}'
+    finish = f'W km: {cale / 1000}'
     return finish
+
+
 if __name__ == "__main__":
+    cal = int(input("Podaj cale: "))
+    cale = 2.54 * cal
     print("Wpisz w co chcesz przekształcić cale: mm,cm,m,km")
-    what = input("Podaj co chcesz ode mnie: ")
-    if what == 'mm':
-        print(mm(cale))
-    elif what == 'cm':
-        print(cm(cale))
-    elif what == 'km':
-        print(km(cale))
-    elif what == 'm':
-        print(m(cale))
+    while True:
+        what = input("Podaj co chcesz ode mnie: ")
+        if what == 'mm':
+            print(mm(cale))
+            break
+        if what == 'cm':
+            print(cm(cale))
+            break
+        if what == 'km':
+            print(km(cale))
+            break
+        if what == 'm':
+            print(m(cale))
+            break
+        else:
+            print('Invalid input. Podaj mm,cm,m,km')
+
